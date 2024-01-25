6551d95d1ec421612fa98ba470c112eb618e2d52
thianecw <thianecw@hotmail.com>
2024-01-25 15:25:31 +0000

    responsividade

---
+++ script.js
@@ -0,0 +1,15 @@
+let formulario = document.querySelector(".fale-conosco")
+console.log(formulario)
+
+let mascara = document.querySelector(".mascara-form")
+
+
+function cliqueiNoBotao(){
+formulario.style.left="700px"
+mascara.style.visibility="visible"
+}
+
+function sumirformulario() {
+formulario.style.letf = "-320px"
+mascara.style.visibility="hidden"
+}
